2fd1592a25b42a9c198b67780255728be2dc26fd
Ariel Rosa <aerosa@comunidad.unnoba.edu.ar>
2020-07-26 00:11:03 -0300

    Agregado de Vista "Administrar Pagos"
    Esta vista todavía no carga los datos de las tablas

--- src/vistas/PanelAdPagos.java
+++ src/vistas/PanelAdPagos.java
@@ -7,7 +7,13 @@ package vistas;
 
 import controladores.ControladorPrincipal;
 import java.awt.Font;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import modelo.JLabelAriel;
 
 /**
  *
@@ -31,12 +37,74 @@ public class PanelAdPagos extends JPanelCustom {
         this.jTablePlanillas.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 15));
         modelo.setColumnIdentifiers(getColumnas());
         this.controlador = ControladorPrincipal.getInstancia();
+        cargarTablaClientes("");
     }
 
     private String[] getColumnas(){
-        String columna[] = new String[]{"Nombre Cliente", "Apellido", "N° Planilla", "¿Impaga?"};
+        String columna[] = new String[]{"Nombre Cliente", "Apellido", "N° Planilla", "¿Impaga?", "IMPORTE"};
+        
         return null;
     }
+    
+    private void cargarTablaClientes(String filtroNombre){
+        //Método para cargar planillas en la tabla
+        //Diseñar para aplicar filtro de nombre de cliente
+        String consulta;
+        if(filtroNombre.equals("")){
+            consulta = "SELECT per.nombre, per.apellido, p.idplanilla, p.pagado FROM persona AS per "
+                    + "INNER JOIN cliente AS c ON per.idpersona = c.idpersona INNER JOIN planilla AS p ON p.idcliente = c.idcliente";
+        }else{
+            consulta = "SELECT per.nombre, per.apellido, p.idplanilla, p.pagado FROM persona AS per "
+                    + "INNER JOIN cliente AS c ON per.idpersona = c.idpersona INNER JOIN planilla AS p ON p.idcliente = c.idcliente "
+                    + "WHERE per.nombre LIKE '"+filtroNombre+"'";
+        }
+        cargarDatosTablaClientes(consulta);
+    }   
+    
+    private void cargarDatosTablaClientes(String consulta){
+        //Método para cargar datos en la tabla una vez que la consulta está lista
+        String[] registro = new String[5];
+        Connection co = this.controlador.obtenerConexion();
+        try {
+            co.setAutoCommit(false);
+            Statement st = co.createStatement();
+            ResultSet rs = st.executeQuery(consulta);
+            while(rs.next()){
+                registro[0] = rs.getString(1); //nombre cliente
+                registro[1] = rs.getString(2); //apellido
+                registro[2] = ""+rs.getInt(3); //Numero planilla
+                if(rs.getBoolean(4)) //Si es verdadero la planilla esta pagada
+                    registro[3] = "NO";
+                else
+                    registro[3] ="SI";
+                registro[4] = ""+cargarImporteReparaciones(rs.getInt(3), co); //Acá tendría que cargar el importe total de las reparaciones.
+                co.commit();
+                this.modelo.addRow(registro);
+                this.jTablePlanillas.updateUI();
+            }
+        } catch (SQLException ex) {
+            JLabelAriel label = new JLabelAriel("Error al cargar tabla de Clientes: "+ex.getMessage());
+            JOptionPane.showMessageDialog(null, label, "ERROR", JOptionPane.ERROR_MESSAGE);
+            try {
+                co.rollback();
+            } catch (SQLException ex1) {
+                //Esto no importaría porque es una consulta de solo lectura
+            }
+        } 
+    }
+    
+    private long cargarImporteReparaciones(int nPlanilla, Connection co) throws SQLException{
+        //Obtiene la suma de los importes de las reparaciones asignadas a la planilla y la retorna
+        long suma = 0;
+        String sql = "SELECT SUM(r.IMPORTE) FROM reparacion AS r WHERE r.idplanilla = '"+nPlanilla+"' ";
+        Statement st = co.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        while(rs.next()){
+            suma+= rs.getLong(1);  //El while solo se ejecuta una vez
+        }
+        return suma;
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -69,9 +137,9 @@ public class PanelAdPagos extends JPanelCustom {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 369, Short.MAX_VALUE)
-                .addGap(138, 138, 138))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(217, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
